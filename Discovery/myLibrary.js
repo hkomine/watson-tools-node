13f9f7dae9a8e27e748d7fa7c2bc3b0a3aa3f9a5
Hiroaki Komine <hkomine@jp.ibm.com>
2017-03-26 23:01:47 +0900

    updates the modules

--- Discovery/myLibrary.js
+++ Discovery/myLibrary.js
@@ -15,15 +15,9 @@ exports.myDiscovery = function(initial_options) {
 		var environment_id = params.environment_id;
 		var configuration_id = params.configuration_id;
 		var response_type = params.response_type;
-/*		var accept_content_type = 'application/wds+xml';
-		if ('binary' === response_type) {
-			accept_content_type = 'application/octet-stream';
-		} else if ('json' === response_type) {
-			accept_content_type = 'application/wds+json';
-		}*/
 
 		var options = {
-			url : base_url +'/v1/environments/' + environment_id + "/configurations/" + configuration_id + '?version=' + initial_options.version_date,
+			url : base_url +'/v1/environments/' + environment_id + '/configurations/' + configuration_id + '?version=' + initial_options.version_date,
 			method : 'DELETE',
 			headers : {
 				'Accept' : 'application/wds+json'
@@ -33,12 +27,37 @@ exports.myDiscovery = function(initial_options) {
 				password : self.password
 			},
 		};
-		
-		console.log (options.url);
 
 		request(options, function(error, response, body) {
 			callback(error, body);
 		});
 	};
+	
+	self.addConfiguration = function(params, callback) {
+		
+		var environment_id = params.environment_id;
+		var configuration = params.configuration;
+		
+		console.log (JSON.stringify(configuration));
+
+		var options = {
+			url : base_url +'/v1/environments/' + environment_id + '/configurations/' + '?version=' + initial_options.version_date,
+			method : 'POST',
+			headers : {
+				'Content-Type' : 'application/json',
+				'Accept' : 'application/wds+json'
+			},
+			auth : {
+				user : self.username,
+				password : self.password
+			},
+			body : JSON.stringify(configuration),
+		};
+		
+		request(options, function(error, response, body) {
+			callback(error, body);
+		});
+	}
+	
 	return self;
 };
