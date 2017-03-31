2ecb43566ad552eb1ede18defeae9f1821aa0f5b
Hiroaki Komine <hkomine@jp.ibm.com>
2017-03-31 21:09:11 +0900

    Implements /preview API

--- Discovery/myLibrary.js
+++ Discovery/myLibrary.js
@@ -1,10 +1,9 @@
 var request = require('request');
 
-var base_url = "https://gateway.watsonplatform.net/discovery/api";
-
 exports.myDiscovery = function(initial_options) {
 	var self = {};
 
+	self.base_url = initial_options.url;
 	self.username = initial_options.username;
 	self.password = initial_options.password;
 	self.version = initial_options.version;
@@ -17,7 +16,7 @@ exports.myDiscovery = function(initial_options) {
 		var response_type = params.response_type;
 
 		var options = {
-			url : base_url +'/v1/environments/' + environment_id + '/configurations/' + configuration_id + '?version=' + initial_options.version_date,
+			url : self.base_url +'/v1/environments/' + environment_id + '/configurations/' + configuration_id + '?version=' + initial_options.version_date,
 			method : 'DELETE',
 			headers : {
 				'Accept' : 'application/wds+json'
@@ -29,7 +28,7 @@ exports.myDiscovery = function(initial_options) {
 		};
 
 		request(options, function(error, response, body) {
-			callback(error, body);
+			callback(error, JSON.parse(body));
 		});
 	};
 	
@@ -38,10 +37,8 @@ exports.myDiscovery = function(initial_options) {
 		var environment_id = params.environment_id;
 		var configuration = params.configuration;
 		
-		console.log (JSON.stringify(configuration));
-
 		var options = {
-			url : base_url +'/v1/environments/' + environment_id + '/configurations/' + '?version=' + initial_options.version_date,
+			url : self.base_url +'/v1/environments/' + environment_id + '/configurations?version=' + initial_options.version_date,
 			method : 'POST',
 			headers : {
 				'Content-Type' : 'application/json',
@@ -55,8 +52,39 @@ exports.myDiscovery = function(initial_options) {
 		};
 		
 		request(options, function(error, response, body) {
-			callback(error, body);
+			callback(error, JSON.parse(body));
 		});
+	}
+	
+	self.preview = function(params, callback) {
+		var environment_id = params.environment_id;
+		var configuration_id = params.configuration_id;
+		var html = params.html;
+		
+		var url = self.base_url +'/v1/environments/' + environment_id + '/preview?configuration_id=' + configuration_id + '&version=' + initial_options.version_date;
+
+		var options = {
+			url : url,
+			method : 'POST',
+			headers : {
+				'Content-Type' : 'multipart/form-data',
+				'Accept' : 'application/wds+json'
+			},
+			auth : {
+				user : self.username,
+				password : self.password
+			}
+		};
+		
+		var req = request.post(options, function(error, response, body) {
+			callback(error, JSON.parse(body));
+		});
+		
+		var form = req.form();
+		form.append('file', html, {
+			filename: '_',
+			contentType : 'text/html'
+		})
 	}
 	
 	return self;
